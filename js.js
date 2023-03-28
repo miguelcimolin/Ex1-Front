9bb6886aba7aa1abb621193533e4bd3be1971a06
Miguel Cimolin <69534716+Miguel-Cimolin@users.noreply.github.com>
2023-03-28 20:46:26 -0300

    2.0
    2.0

--- js.js
+++ js.js
@@ -1,17 +1,24 @@
 function adicionarMsg(inputMessage) {
-    const novoItem = document.createElement("p"); //cria a "base" da nova mensagem <p>
-    novoItem.style.width = '100px';
-    novoItem.style.height = '120px';
-    
-    const text = document.createTextNode(inputMessage); //adiciona o texto dentro da "base"
-    novoItem.appendChild(text);
+    if(inputMessage != ''){
+        const novoItem = document.createElement("p"); //cria a "base" da nova mensagem <p>
+        novoItem.innerHTML = `${inputMessage}`;
+        novoItem.className = 'chat-settings'; //busca o styles do css
 
-    const newComment = document.querySelector("#newMessages"); //usa append pra jogar dentro da div, selecionada pela classe
-    newComment.appendChild(novoItem);
+        const newComment = document.querySelector("#newMessages"); //usa append pra jogar dentro da div, selecionada pela classe
+        newComment.appendChild(novoItem);
+        const box2 = document.querySelector('#box2');
+        box2.scrollTop = box2.scrollHeight; //faz a barra de scroll seguir
+
+        document.getElementById('message').value = '' //limpa o placeholder
+    } else {
+        alert('Digite uma mensagem valida.')
+        document.getElementById('message').value = '' //limpa o placeholder
+    }
 }
 
 const formAdd = document.querySelector("#send-message-box"); //monitora o enter
 formAdd.addEventListener("submit", function(event) {
+    event.preventDefault();
     if (True) {
         const inputMsg = document.getElementById('message').value
         adicionarMsg(inputMsg);
